a08fe8db950240422e8689ff62ff452c85339255
Simon Mika <simonmika@users.noreply.github.com>
2019-09-24 14:02:30 +0200

    First working version. (#1)

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
+import { Flags } from "./Flags"
+import { parse } from "./parse"
+import { get } from "./get"
+import { set } from "./set"
+import { unset } from "./unset"
+
 export {
+	Flags,
+	parse,
+	get,
+	set,
+	unset,
 }
