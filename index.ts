a1e931e56cb3f24cb195baa951a57ed72374ee6d
Emma Nilsson <31855183+nilssonemma@users.noreply.github.com>
2020-08-18 23:36:16 +0200

    Changed to eslint and prettierx. (#6)
    * Changed to eslint and prettierx.
    
    * Fixed newline at end of files.
    
    Co-authored-by: Emma Nilsson <emma.nilsson@payfunc.com>

--- index.ts
+++ index.ts
@@ -4,10 +4,4 @@ import { get } from "./get"
 import { set } from "./set"
 import { unset } from "./unset"
 
-export {
-	Flags,
-	parse,
-	get,
-	set,
-	unset,
-}
+export { Flags, parse, get, set, unset }
