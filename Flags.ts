ee7fc66b478566f2f7a0af66a5c78d4910cdd54c
Florian Ebmeier <florian.ebmeier@payfunc.com>
2020-10-27 16:11:06 +0100

    Implement stringify

--- Flags.ts
+++ Flags.ts
@@ -6,4 +6,21 @@ export namespace Flags {
 	export function is(value: Flags | any): value is Flags {
 		return typeof value == "object" && Object.values(value).every(v => typeof v == "boolean" || v == undefined || is(v))
 	}
+
+	export function stringify(value: Flags): string {
+		return Object.entries(getBaseKey(value))
+			.reduce<string>((r, c) => r + (c[1] ? "+" : "-") + c[0] + " ", "")
+			.trim()
+	}
+}
+
+function getBaseKey(value: Flags): Record<string, boolean> {
+	const result: Record<string, boolean> = {}
+	for (const key of Object.keys(value)) {
+		if (Flags.is(value[key]))
+			Object.entries(getBaseKey(value[key] as Flags)).forEach(entry => (result[key + "." + entry[0]] = entry[1]))
+		else if (typeof value[key] == "boolean")
+			result[key] = value[key] as boolean
+	}
+	return result
 }
---
+++ index.spec.ts
@@ -0,0 +1,9 @@
+import * as flagly from "./index"
+
+describe("flagly", () => {
+	it("stringify", () => {
+		const testString = "+a.b.c +a.b.d +e.s -test"
+		const flags = flagly.parse(testString)
+		expect(flagly.Flags.stringify(flags)).toEqual(testString)
+	})
+})
