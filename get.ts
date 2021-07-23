2b63eee2804cfec21a990283d3633ba81bf8b9de
marcushanses <79835961+marcushanses@users.noreply.github.com>
2021-07-23 08:59:26 +0200

    Allowed get to handle undefined values. (#23)

--- get.ts
+++ get.ts
@@ -1,6 +1,6 @@
 import { Flags } from "./Flags"
 
-export function get(flags: Readonly<Flags>, ...flag: string[]): Flags | boolean | undefined {
-	const next = flags[flag[0]]
+export function get(flags?: Readonly<Flags>, ...flag: string[]): Flags | boolean | undefined {
+	const next = flags?.[flag[0]]
 	return flag.length == 1 ? next : typeof next == "object" ? get(next, ...flag.slice(1)) : undefined
 }
