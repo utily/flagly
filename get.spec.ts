d83013d0346ddf2cb32c4171e57260128b80f8d4
Elias Eriksson <eliaseriksson95@gmail.com>
2023-08-03 09:48:33 +0200

    test for get with flag named undefined

--- get.spec.ts
+++ get.spec.ts
@@ -7,4 +7,5 @@ describe("flagly.get", () => {
 	it("false", () => expect(flagly.get({ simple: false }, "simple")).toEqual(false))
 	it("undefined", () => expect(flagly.get(undefined, "simple")).toEqual(undefined))
 	it("no flag", () => expect(flagly.get({ simple: true }, ...[])).toEqual(undefined))
+	it("undefined flag", () => expect(flagly.get({ undefined: true }, ...[])).toEqual(undefined))
 })
