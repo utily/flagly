94d4e9d7b3c79ffd227320d79312d7e9f2ad6fca
Elias Eriksson <eliaseriksson95@gmail.com>
2023-08-03 09:53:41 +0200

    linting

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Flags } from "./Flags"
-import { parse } from "./parse"
 import { get } from "./get"
+import { parse } from "./parse"
 import { reduce } from "./reduce"
 import { set } from "./set"
 import { unset } from "./unset"
