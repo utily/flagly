1b093c421605c2ac2a253210f175e15e2d35cd49
Elias Eriksson <eliaseriksson95@gmail.com>
2023-08-03 09:34:42 +0200

    empty flag array fix

--- get.ts
+++ get.ts
@@ -1,6 +1,7 @@
 import { Flags } from "./Flags"
 
 export function get(flags?: Readonly<Flags>, ...flag: string[]): Flags | boolean | undefined {
-	const next = flags?.[flag[0]]
+	const key = flag.at(0)
+	const next = key == undefined ? undefined : flags?.[key]
 	return flag.length == 1 ? next : typeof next == "object" ? get(next, ...flag.slice(1)) : undefined
 }
--- set.ts
+++ set.ts
@@ -1,11 +1,16 @@
 import { Flags } from "./Flags"
 
 export function set(flags: Readonly<Flags>, ...flag: string[]): Flags {
-	const next = flags[flag[0]]
-	const result = { ...flags }
-	if (flag.length > 1)
-		result[flag[0]] = set(typeof next == "object" ? next : {}, ...flag.slice(1))
+	let result: Flags
+	const key = flag.at(0)
+	const next = key == undefined ? undefined : flags[key]
+	if (!key)
+		result = { ...flags }
+	else if (flag.length > 1)
+		result = { ...flags, [key]: set(typeof next == "object" ? next : {}, ...flag.slice(1)) }
 	else if (!next)
-		result[flag[0]] = true
+		result = { ...flags, [key]: true }
+	else
+		result = { ...flags }
 	return result
 }
