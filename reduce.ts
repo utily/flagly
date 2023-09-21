7a83481e96f7c6d0c2cc677331add4a5f6bcf4b7
Elias Eriksson <eliaseriksson95@gmail.com>
2023-09-21 16:32:36 +0200

    prefer right side true over left side object

--- reduce.ts
+++ reduce.ts
@@ -1,14 +1,22 @@
 import { Flags } from "./Flags"
 export function reduce(...flags: Flags[]): Flags {
 	return flags.reduce<Flags>(
-		(r, f) =>
-			Object.entries(f).reduce((result, [flag, value]) => {
+		(result, flag) =>
+			Object.entries(flag).reduce((result, [flag, value]) => {
 				const old = result[flag]
 				return {
 					...result,
-					[flag]: typeof old != "object" ? value : typeof value != "object" ? value && old : reduce(old, value),
+					[flag]:
+						value == true
+							? true
+							: typeof old != "object"
+							? value ?? old
+							: typeof value != "object"
+							? value && old
+							: reduce(old, value),
+					// [flag]: typeof old != "object" ? value : typeof value != "object" ? value && old : reduce(old, value),
 				}
-			}, r),
+			}, result),
 		{}
 	)
 }
