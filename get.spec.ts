f2532daaa67ef61680558b158bf93133703f0b6e
Elias Eriksson <elias@issuefab.com>
2023-10-10 08:40:47 +0200

    nested get (#38)
    * true on higher level is true on lower level.
    
    * some more tests

--- get.spec.ts
+++ get.spec.ts
@@ -6,4 +6,9 @@ describe("flagly.get", () => {
 	it("not set", () => expect(flagly.get({}, "simple")).toEqual(undefined))
 	it("false", () => expect(flagly.get({ simple: false }, "simple")).toEqual(false))
 	it("undefined", () => expect(flagly.get(undefined, "simple")).toEqual(undefined))
+	it("nested", () => {
+		expect(flagly.get({ a: true }, "a", "b")).toEqual(true)
+		expect(flagly.get({ a: true, b: { c: true } }, "b", "d")).toEqual(undefined)
+		expect(flagly.get({ a: true, b: { c: true } }, "b", "c", "d")).toEqual(true)
+	})
 })
