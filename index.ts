4903b68ec5d4ec3b837bb51a6a05652acce6ad03
Simon Mika <simon@mika.se>
2024-12-02 18:37:39 +0100

    Added missing flatten export.

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-export * as flagly from "./flagly"
+import { Flags as flaglyFlags } from "./Flags"
+import { flatten as flaglyFlatten } from "./flatten"
+import { get as flaglyGet } from "./get"
+import { has as flaglyHas } from "./has"
+import { merge as flaglyMerge } from "./merge"
+import { parse as flaglyParse } from "./parse"
+import { reduce as flaglyReduce } from "./reduce"
+import { remove as flaglyRemove } from "./remove"
+import { set as flaglySet } from "./set"
+import { stringify as flaglyStringify } from "./stringify"
+import { unset as flaglyUnset } from "./unset"
+
+export namespace flagly {
+	export import Flags = flaglyFlags
+	export const flatten = flaglyFlatten
+	export const get = flaglyGet
+	export const has = flaglyHas
+	export const merge = flaglyMerge
+	export const parse = flaglyParse
+	export const reduce = flaglyReduce
+	export const remove = flaglyRemove
+	export const set = flaglySet
+	export const stringify = flaglyStringify
+	export const unset = flaglyUnset
+}
