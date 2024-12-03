fe666f8e34f8ce5ae3fc6f8caf0bc329c95748cc
Simon Mika <simonmika@users.noreply.github.com>
2024-12-03 14:46:09 +0100

    Flatten export (#44)
    *   exported flatten
    *  simplified set
    *  removed flagly.ts

--- set.ts
+++ set.ts
@@ -1,18 +1,13 @@
 import { Flags } from "./Flags"
 export const set = Object.assign(setFlags, { path: setPaths })
 function setFlags(flags: Readonly<Flags>, ...flag: string[]): Flags {
-	let result: Flags
 	const key = flag.at(0)
 	const next = key == undefined ? undefined : flags[key]
-	if (!key)
-		result = { ...flags }
-	else if (flag.length > 1)
-		result = { ...flags, [key]: set(typeof next == "object" ? next : {}, ...flag.slice(1)) }
-	else if (!next)
-		result = { ...flags, [key]: true }
-	else
-		result = { ...flags }
-	return result
+	return key && flag.length > 1
+		? { ...flags, [key]: set(typeof next == "object" ? next : {}, ...flag.slice(1)) }
+		: key && !next
+		? { ...flags, [key]: true }
+		: { ...flags }
 }
 function setPaths(flags: Readonly<Flags>, ...paths: string[]) {
 	return paths.reduce((result, path) => set(result, ...path.split(".")), flags)
