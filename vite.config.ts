afd7a9433f3318d4de8480617d36ecd5291bcbe0
Anton Kroner <anton.kroner@pax2pay.com>
2025-02-20 11:41:48 +0100

    Updated config and switched to vitest.

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite"
+
+export default defineConfig({
+	test: {
+		typecheck: { tsconfig: "./tsconfig.json" },
+		coverage: {
+			reporter: ["text", "json", "html"],
+			enabled: true,
+			all: true,
+			cleanOnRerun: true,
+			thresholds: { statements: 90, branches: 90, functions: 90, lines: 90 },
+		},
+		globals: true,
+		include: ["**/*.spec.[tj]s"],
+		testTimeout: 20000,
+		isolate: false,
+		exclude: ["node_modules", "dist"],
+		server: { deps: { inline: ["isly"] } },
+	},
+})
